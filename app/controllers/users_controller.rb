19cf1efd2e38fa51b03ff6589c4b8658b69639de
Kris <weldon.kris@gmail.com>
2016-03-01 16:46:47 -0500

    signup working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
-    # if @user.save
-    #   flash[:notice] = "success"
+    if @user.save
+      flash[:notice] = "success"
+      redirect_to user_path @user
+    else
+      flash[:alert] = "Didn't work"
+      redirect_to 'user/new' #did this work?
+    end
   end
-
+def show
+  @user = User.find(params[:id])
+end
 
 end
